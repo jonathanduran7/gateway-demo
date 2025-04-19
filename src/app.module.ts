387365b6b1017c5bb1415f5ff1aaa9f9aae429be
jonathanduran7 <joniduran624@gmail.com>
2025-04-18 23:30:12 -0300

    Enhance AppModule by adding HttpModule and integrating UsersProxy and ProductsProxy as controllers. This improves the application's ability to handle HTTP requests and manage user and product-related operations.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { UsersProxy } from './proxy/users.proxy';
+import { ProductsProxy } from './proxy/products.proxy';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
+  imports: [
+    HttpModule,
+    AuthModule,
+  ],
+  controllers: [AppController, UsersProxy, ProductsProxy],
   providers: [AppService],
 })
 export class AppModule {}
