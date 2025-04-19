8df86da156f4cc9dc40bc694dd9c93d189ec74a1
jonathanduran7 <joniduran624@gmail.com>
2025-04-18 23:29:56 -0300

    Refactor UsersProxy to improve error handling and add a new forwarding method. The root method now handles requests with enhanced error management, and a separate forward method is introduced for handling requests with a wildcard path.

--- src/proxy/users.proxy.ts
+++ src/proxy/users.proxy.ts
@@ -1,21 +1,54 @@
-import { All, UseGuards, Req } from "@nestjs/common";
+import { All, UseGuards, Req, HttpStatus, HttpException, Get, Param } from "@nestjs/common";
 import { HttpService } from "@nestjs/axios";
 import { Controller } from "@nestjs/common";
 import { JwtAuthGuard } from "src/auth/jwt.strategy";
 import { Request } from 'express';
+import { firstValueFrom } from "rxjs";
 
 @Controller('users')
-@UseGuards(JwtAuthGuard)
 export class UsersProxy {
   constructor(private readonly http: HttpService) {}
 
-  @All('*')
-  async proxy(@Req() req: Request) {
-    return this.http.axiosRef({
-      url: `http://users:3000${req.originalUrl}`,
-      method: req.method,
-      data: req.body,
-      headers: { Authorization: req.headers.authorization },
-    }).then(res => res.data);
+  @All()
+  async root(@Req() req: Request) {
+    try {
+      const targetUrl = `http://users:3000${req.originalUrl}`;
+      const response = await this.http.axiosRef({
+        url: targetUrl,
+        method: req.method,
+        data: req.body,
+        headers: {
+          host: undefined,
+        },
+      })
+    
+    return response.data;
+    } catch (err: any) {
+      const status = err?.response?.status ?? HttpStatus.INTERNAL_SERVER_ERROR;
+      const message = err?.response?.data?.message ?? 'Error forwarding to users-service';
+      throw new HttpException(message, status);
+    }
+  }
+
+  @All('*path')
+  async forward(@Req() req: Request) {
+    try {
+      const targetUrl = `http://users:3000${req.originalUrl}`;
+      const response = await this.http.axiosRef({
+          url: targetUrl,
+          method: req.method,
+          data: req.body,
+          headers: {
+            host: undefined,
+          },
+        })
+      
+      return response.data;
+    } catch (err: any) {
+      console.error('Proxy error:', err.message);
+      const status = err?.response?.status ?? HttpStatus.INTERNAL_SERVER_ERROR;
+      const message = err?.response?.data?.message ?? 'Error forwarding to users-service';
+      throw new HttpException(message, status);
+    }
   }
 }
